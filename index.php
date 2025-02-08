f26c461ab7602a89b811115bdad2a1c2fcab096b
Sadra <123418962+SadraGt@users.noreply.github.com>
2025-02-08 13:27:56 +0330

    Add files via upload

--- index.php
+++ index.php
@@ -4,45 +4,31 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>info</title>
-  <!-- <link rel="stylesheet" href="styles.css"> -->
+  <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-<!-- PaymentMethod in iran -->
-  <form action="index.php" method="post">
-    <input type="checkbox" name="pizza" value="Pizza" >pizza<br>
-    <input type="checkbox" name="hotdog" value="Hotdog">hotdogs<br>
-    <input type="checkbox" name="hamburger" value="Hamburger" >hamburgers<br>
-    <input type="submit" name="submit">
-
-  </form>
-
+  <div class="form-container">
+    <form action="index.php" method="post">
+      <h2>Info</h2>
+      <div class="form-group">
+        <label for="name">Name</label>
+        <input type="text" id="name" name="username" placeholder="Enter name" required>
+      </div>
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email" placeholder="Enter email" required>
+      </div>
+      <div class="form-group">
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" placeholder="Enter password" required>
+      </div>
+      
+      <input type="submit" class="submit-btn" value="Log in">
+    </form>
+  </div>
 </body>
 </html>
-
-
 <?php
-
-if (isset($_POST["submit"])){
-
-  if (isset($_POST["pizza"])) {
-    echo "you like pizza! <br>";
-  }
-  if (isset($_POST["hotdog"])) {
-    echo "you like hotdog! <br>";
-  }
-  if (isset($_POST["hamburger"])) {
-    echo "you like hamburger! <br>";
-  }elseif (empty($_POST["hamburger"])) {
-    echo "you don't like hamburger <br>";
-  }
-  if (empty($_POST["pizza"])){
-    echo "you don't like pizza <br>";
-  }
-  if (empty($_POST["hotdog"])){
-    echo "you don't like hotdog <br>";
-  }
- 
-}
-
-  
+    echo $_POST["username"] . "<br>";
+    echo "{$_POST["email"]} <br>";
 ?>
